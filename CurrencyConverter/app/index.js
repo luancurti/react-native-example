4e0ff8ac71a437fe818ad189c67e786cabf60372
luancurti <luancurti@gmail.com>
2018-03-10 21:04:28 -0300

    Added index.js in app folder to startup

---
+++ CurrencyConverter/app/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          Open up index.js to start working on your app!
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff'
+  }
+});
